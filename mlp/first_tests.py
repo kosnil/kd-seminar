3c720816ac8a25c15eec277b45bc330fbeed149a
Nils Koster <nils.koster@t-online.de>
2018-05-23 15:12:23 +0200

    Notebook ist erstes working example. Viel durcheinander, aber funktioniert soweit

---
+++ mlp/first_tests.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+
+import os
+wd = os.getcwd()
+print(wd)
+
+df = pd.read_csv("complete_data.csv")
+df.head()
+
+# dims = X_train.shape[1]
+# print(dims, 'dims')
+# print("Building model...")
+#
+# nb_classes = Y_train.shape[1]
+# print(nb_classes, 'classes')
+#
+# model = Sequential()
+# model.add(Dense(nb_classes, input_shape=(dims,), activation='sigmoid'))
+# model.add(Activation('softmax'))
+#
+# model.compile(optimizer='sgd', loss='categorical_crossentropy')
+# model.fit(X_train, Y_train)
