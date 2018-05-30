48bf316acb6315bb8a4741a6e6b531d026677b08
daniela <daniela.kaun@gmx.de>
2018-05-30 10:43:25 +0200

    adding er features

--- ibm/tone_ibm.py
+++ ibm/tone_ibm.py
@@ -27,8 +27,8 @@ def getSentiment(article):
     start = time.time()
     tone_analyzer = ToneAnalyzerV3(
         version='2017-09-21',
-        username="a0eef1a1-2775-4211-af6b-cca6c89d8693",
-        password="LFXlfgem2kZZ",
+        username="c80a8e92-463d-44f6-8f59-0361510df470",
+        password="154pwVYbbWJD",
         url="https://gateway.watsonplatform.net/tone-analyzer/api"
     )
 
