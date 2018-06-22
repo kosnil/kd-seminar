b559e1c8739c57abc93b10ba590b94be56f3edda
daniela <daniela.kaun@gmx.de>
2018-06-22 08:55:33 +0200

    18.02.18 - 30.04.18 - body of articles

--- er_body_data/get_article_bodies.py
+++ er_body_data/get_article_bodies.py
@@ -14,8 +14,8 @@ analytics = ER.Analytics(er)
 companies = ['Samsung', 'BASF', 'Apple', 'Tesla', 'Airbus', 'Bayer', 'BMW', 'Telefonica', 'Google', 'Allianz', 'Total']
 
 # DEFINE start and end date
-startDate = datetime.date(2018, 3, 20)
-endDate = datetime.date(2018, 4, 30)
+startDate = datetime.date(2018, 2, 12)
+endDate = datetime.date(2018, 2, 17)
 # Get all Business Days in Period
 time_frame = pd.bdate_range(startDate, endDate)
 
